9228daf2ba567fc262ad3135cab41208a7cea208
Richelle <richelle.ak@gmail.com>
2023-02-24 17:12:42 -0800

    sencond test pass

--- VendorOrder.Tests/ModelTests/OrderTests.cs
+++ VendorOrder.Tests/ModelTests/OrderTests.cs
@@ -13,6 +13,15 @@ namespace OrderTracker.Tests
       Order newOrder = new Order("order1");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    [TestMethod]
+
+    public void GetTitle_ReturnsTitle_Title()
+    {
+      string title = "order1";
+      Order newOrder = new Order (title);
+      Assert.AreEqual(newOrder.Title, title);
+    }
   }
 
 }
