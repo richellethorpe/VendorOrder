d5f0590b4e523420a25ce031c2c26f07a19aaf9b
Richelle <richelle.ak@gmail.com>
2023-02-25 18:53:38 -0800

    add dispose method to testing and pass test for getall method

--- VendorOrder.Tests/ModelTests/OrderTests.cs
+++ VendorOrder.Tests/ModelTests/OrderTests.cs
@@ -1,16 +1,24 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OrderTracker.Models;
+using System.Collections.Generic;
+using System;
+
 
 namespace OrderTracker.Tests
 {
   [TestClass]
 
-  public class OrderTests
+  public class OrderTests : IDisposable
   {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+    
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      Order newOrder = new Order("order1");
+      Order newOrder = new Order("order1", "test", 2, "test");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
 
@@ -19,8 +27,27 @@ namespace OrderTracker.Tests
     public void GetTitle_ReturnsTitle_Title()
     {
       string title = "order1";
-      Order newOrder = new Order (title);
+      string description = "a dozen muffins";
+      double price= 12.50;
+      string date = "March 5th";
+      Order newOrder = new Order (title, description, price, date);
       Assert.AreEqual(newOrder.Title, title);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      string title = "order1";
+      string title2 = "order2";
+      string description = "a dozen muffins";
+      double price= 12.50;
+      string date = "March 5th";
+      Order newOrder = new Order (title, description, price, date);
+      Order newOrder2 = new Order (title2, description, price, date);
+      List<Order> newList  = new List<Order> { newOrder, newOrder2 };
+      List<Order> result = Order.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+      
     }
   }
 
